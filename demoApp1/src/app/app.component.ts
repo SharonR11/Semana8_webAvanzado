b2e9144b0487e14dd06366749852110b860480ab
SharonR11 <sharon.rudas@tecsup.edu.pe>
2023-10-27 22:37:36 -0500

    listadoarticulos

--- demoApp1/src/app/app.component.ts
+++ demoApp1/src/app/app.component.ts
@@ -7,7 +7,14 @@ import { SelectornumericoComponent } from './selectornumerico/selectornumerico.c
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  
+  productos = [
+    {codigo:1, descripcion: 'coronita', precio: '0.50'},
+    {codigo:2, descripcion: 'incakola', precio: '2.50'},
+    {codigo:3, descripcion: 'cocacola', precio: '2.50'},
+    {codigo:4, descripcion: 'cañonazo', precio: '0.70'},
+    {codigo:5, descripcion: 'agua cielo', precio: '1.20'}
+  ];
+
   @ViewChild('selector2') selector: 
   SelectornumericoComponent = new SelectornumericoComponent();
   fijarSelector2(valor: number){
--- demoApp1/src/app/app.module.ts
+++ demoApp1/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SelectornumericoComponent } from './selectornumerico/selectornumerico.component';
+import { ListadoarticulosComponent } from './listadoarticulos/listadoarticulos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SelectornumericoComponent
+    SelectornumericoComponent,
+    ListadoarticulosComponent
   ],
   imports: [
     BrowserModule,
