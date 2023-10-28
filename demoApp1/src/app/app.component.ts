0a3eb6011b52922141756176de5e921fe4c2f41c
SharonR11 <sharon.rudas@tecsup.edu.pe>
2023-10-27 22:19:50 -0500

    selector 2

--- demoApp1/src/app/app.component.ts
+++ demoApp1/src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
-
+import { ViewChild } from '@angular/core';
+import { SelectornumericoComponent } from './selectornumerico/selectornumerico.component';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'demoApp1';
+  
+  @ViewChild('selector2') selector: 
+  SelectornumericoComponent = new SelectornumericoComponent();
+  fijarSelector2(valor: number){
+    this.selector.fijar(valor);
+}
 }
